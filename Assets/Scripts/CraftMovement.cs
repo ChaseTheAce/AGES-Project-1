ac51f5f937eec073430c36e5d7dd16844e3d210b
ChaseTheAce <32078056+ChaseTheAce@users.noreply.github.com>
2018-03-03 16:58:00 -0600

    Player Movement and Model

--- Assets/Scripts/CraftMovement.cs
+++ Assets/Scripts/CraftMovement.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class CraftMovement : MonoBehaviour 
 {
-	// Use this for initialization
-	void Start () 
+    public int playerNumber = 1;
+    public float speed = 12f;
+    public float turnSpeed = 180f;
+
+    private string movementAxisName;
+    private string turnAxisName;
+    private Rigidbody myRigidbody;
+    private float movementInputValue;
+    private float turnInputValue;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        myRigidbody.isKinematic = false;
+        movementInputValue = 0f;
+        turnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        myRigidbody.isKinematic = true;
+    }
+
+    // Use this for initialization
+    void Start () 
 	{
-		
-	}
+        movementAxisName = "Vertical" + playerNumber;
+        turnAxisName = "Horizontal" + playerNumber;
+    }
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+        movementInputValue = Input.GetAxis(movementAxisName);
+        turnInputValue = Input.GetAxis(turnAxisName);
 	}
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = transform.forward * movementInputValue * speed * Time.deltaTime;
+
+        myRigidbody.MovePosition(myRigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        float turn = turnInputValue * turnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        myRigidbody.MoveRotation(myRigidbody.rotation * turnRotation);
+    }
 }
